baf5bd03b89e869a93065de303b49200e3b4693b
Smitkumar Panchal <52967650+Smit271@users.noreply.github.com>
2021-03-16 15:56:34 +0530

    commit 2
    Added OOPs concept

--- Amazon_automation.py
+++ Amazon_automation.py
@@ -10,88 +10,96 @@ search_item = input("What item (Use + for spaces): ") # Product Name
 page_no = int(input("Till how many pages : "))        # Number of Pages you want to extract
 
 
-# Method for get data from Amazon
-def get_data(search_item, page_no):
-    url = f"https://www.amazon.in/s?k={search_item}&page={page_no}"
-    r = s.get(url)
-    r.html.render(sleep = 1)
-    soup = BeautifulSoup(r.html.html, 'html.parser')
-    return soup
+class Amazon_automation():
+    def __init__(self, search_item, page_no):
+        self.search_item = search_item
+        self.page_no = page_no
+
+    # Method for get data from Amazon
+    def get_data(self, search_item, page_no):
+        url = f"https://www.amazon.in/s?k={search_item}&page={page_no}"
+        r = s.get(url)
+        r.html.render(sleep = 1)
+        soup = BeautifulSoup(r.html.html, 'html.parser')
+        return soup
 
 
-# Method to store in local database
-def store_in_db(title, price, link, rating, review):
+    # Method to store in local database
+    def store_in_db(self, title, price, link, rating, review):
 
-    # Making databse as name of search
-    database_path = f"{search_item}.db"
+        # Making databse as name of search
+        database_path = f"{search_item}.db"
 
-    conn = sqlite3.Connection(database_path)
-    c = conn.cursor()
+        conn = sqlite3.Connection(database_path)
+        c = conn.cursor()
 
-    #c.execute(f'DROP TABLE IF EXISTS {database_path}')
-    c.execute(f'''CREATE TABLE IF NOT EXISTS {search_item.split('+')[0].lower()} (name text, price text,
-               link text, rating real, reviews real)''')
+        #c.execute(f'DROP TABLE IF EXISTS {database_path}')
+        c.execute(f'''CREATE TABLE IF NOT EXISTS {search_item.split('+')[0].lower()} (name text, price text,
+                   link text, rating real, reviews real)''')
 
-    # Insert a row of data
-    insert_query = f"INSERT OR IGNORE INTO {search_item.split('+')[0].lower()} VALUES (?, ?, ?, ?, ?)"
-    c.execute(insert_query, (title, price, link, rating, review))
+        # Insert a row of data
+        insert_query = f"INSERT OR IGNORE INTO {search_item.split('+')[0].lower()} VALUES (?, ?, ?, ?, ?)"
+        c.execute(insert_query, (title, price, link, rating, review))
 
-    conn.commit()
-    conn.close()
-
-
-# Method to get all details of desired product
-def get_details(soup):
-    names, links, prices, ratings, reviews = [], [], [], [], []
-    products = soup.find_all('div', {'data-component-type' : 's-search-result'})
-    for item in products:
-        link = item.find('a', {'class' : 'a-link-normal a-text-normal'})['href']
-        name = item.find('a', {'class' : 'a-link-normal a-text-normal'}).text.strip()
-
-        try:
-            price = float(item.find('span', {'class' : 'a-offscreen'}).text.replace('₹','').replace(',',''))
-        except:
-            price = 00
-
-        try:
-            rating = item.find('span', {'class' : "a-icon-alt"}).text.strip()
-        except:
-            rating = 0
-
-        try:
-            review = item.find('span', {'class' : 'a-size-base'}).text.strip()
-        except:
-            review = 0
-
-        # Now let's store details into our local sqlite database
-        store_in_db(name, price, baseurl + link, rating, review)
-
-        # To make Dict let's make lists of Product details
-        names.append(name)
-        links.append(baseurl+link)
-        prices.append(price)
-        ratings.append(rating)
-        reviews.append(review)
-
-    # Let's make dictionary if in future we want to make DataFrame
-    product_item = {
-        'title' : names,
-        'price' : prices,
-        'link' : links,
-        'ratings' : ratings,
-        'reviews' : reviews
-    }
-    return product_item
+        conn.commit()
+        conn.close()
 
 
-def main():
-    i = 1 # To iterate through pages    
-    while i <= page_no:
-        deal = get_details(get_data(search_item, i))
-        print(f"Completed page number : {i}")
-        i += 1
-    print("Finished, Please check Database file")
-    #print(deal) # To print Dict of data
+    # Method to get all details of desired product
+    def get_details(self, soup):
+        names, links, prices, ratings, reviews = [], [], [], [], []
+        products = soup.find_all('div', {'data-component-type' : 's-search-result'})
+        for item in products:
+            link = item.find('a', {'class' : 'a-link-normal a-text-normal'})['href']
+            name = item.find('a', {'class' : 'a-link-normal a-text-normal'}).text.strip()
+
+            try:
+                price = float(item.find('span', {'class' : 'a-offscreen'}).text.replace('₹','').replace(',',''))
+            except:
+                price = 00
+
+            try:
+                rating = item.find('span', {'class' : "a-icon-alt"}).text.strip()
+            except:
+                rating = 0
+
+            try:
+                review = item.find('span', {'class' : 'a-size-base'}).text.strip()
+            except:
+                review = 0
+
+            # Now let's store details into our local sqlite database
+            self.store_in_db(name, price, baseurl + link, rating, review)
+
+            # To make Dict let's make lists of Product details
+            names.append(name)
+            links.append(baseurl+link)
+            prices.append(price)
+            ratings.append(rating)
+            reviews.append(review)
+
+        # Let's make dictionary if in future we want to make DataFrame
+        product_item = {
+            'title' : names,
+            'price' : prices,
+            'link' : links,
+            'ratings' : ratings,
+            'reviews' : reviews
+        }
+        return product_item
 
 
-main()
+    def main(self):
+        i = 1 # To iterate through pages
+        while i <= page_no:
+            deal = self.get_details(self.get_data(search_item, i))
+            print(f"Completed page number : {i}\tItem Counts : {len(deal['title'])}")
+            i += 1
+        print("Finished, Please check Database file")
+        #print(deal) # To print Dict of data
+
+
+# Creating Object of Amazon_automation class
+amazon = Amazon_automation(search_item, page_no)
+# Calling main method of object
+amazon.main()
